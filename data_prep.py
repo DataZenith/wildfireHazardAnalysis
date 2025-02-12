3eed0599745af88921d791ae18d566d6ffc9caad
Bigworm <jkauwe@yahoo.com>
2025-02-12 08:07:39 -0800

    Update data_prep.py

--- data_prep.py
+++ data_prep.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('D:/fire_data/final_analysis/scrub_data.csv')
+path = #insert path to downloaded data here
+df = pd.read_csv(path)
 
 df['extreme'] = df["hazard_1"].apply(lambda x: 1 if  x > 0.137872 else 0)
 df['moderate'] = df["hazard_1"].apply(lambda x: 1 if .001911 < x <= .137872 else 0)
